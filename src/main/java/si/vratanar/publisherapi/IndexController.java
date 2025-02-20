db4e6da90d4c72490218349211eea90271d760a8
jvratanar <jvratanar@gmail.com>
2025-02-20 18:06:48 +0100

    Connecting to the database.

---
+++ src/main/java/si/vratanar/publisherapi/IndexController.java
@@ -0,0 +1,12 @@
+package si.vratanar.publisherapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/index")
+public class IndexController {
+    @GetMapping()
+    public String index() {
+        return "Hello World!";
+    }
+}
