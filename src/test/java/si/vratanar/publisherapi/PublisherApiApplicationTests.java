a97363dd096dfe172c5e3baccae0f65d9a2d0fb7
jvratanar <jvratanar@gmail.com>
2025-02-20 17:13:31 +0100

    Initital commit

---
+++ src/test/java/si/vratanar/publisherapi/PublisherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package si.vratanar.publisherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
